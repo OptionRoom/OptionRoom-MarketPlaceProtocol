b798ace6c30e70c39d8e98ad1c72b6a26430b7eb
Tareq doufish <boobish@gmail.comd>
2021-04-05 00:49:15 +0300

    .

--- contracts/AppDependencies.sol
+++ contracts/AppDependencies.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.1;
 // NOTE: This file porpouse is just to make sure truffle compiles all of depending
 //  contracts when we are in development.
 
-//import '@gnosis.pm/conditional-tokens-market-makers/contracts/LMSRMarketMaker.sol';
-//import '@gnosis.pm/conditional-tokens-market-makers/contracts/LMSRMarketMakerFactory.sol';
+import '@gnosis.pm/conditional-tokens-market-makers/contracts/LMSRMarketMaker.sol';
+import '@gnosis.pm/conditional-tokens-market-makers/contracts/LMSRMarketMakerFactory.sol';
 
 //import './ExtendedMarketFactory.sol';
 //import './FixedProductMarketMaker.sol';
--- contracts/ExtendedMarketMakert.sol
+++ contracts/ExtendedMarketMakert.sol
@@ -1,61 +1,61 @@
-pragma solidity ^0.5.1;
-import {FixedProductMarketMaker} from "gnosis.pm/conditional-tokens-market-makers/contracts/FixedProductMarketMaker.sol";
-import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import { ConditionalTokens } from "gnosis.pm/conditional-tokens-contracts/contracts/ConditionalTokens.sol";
-import { CTHelpers } from "gnosis.pm/conditional-tokens-contracts/contracts/CTHelpers.sol";
-import { ERC1155TokenReceiver } from "gnosis.pm/conditional-tokens-contracts/contracts/ERC1155/ERC1155TokenReceiver.sol";
-import { ERC20 } from "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-
-contract ExtendedMarketMakert is FixedProductMarketMaker {
-}
-
-// for proxying purposes
-contract FixedProductMarketMakerData {
-    mapping (address => uint256) internal _balances;
-    mapping (address => mapping (address => uint256)) internal _allowances;
-    uint256 internal _totalSupply;
-
-
-    bytes4 internal constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
-    mapping(bytes4 => bool) internal _supportedInterfaces;
-
-
-    event FPMMFundingAdded(
-        address indexed funder,
-        uint[] amountsAdded,
-        uint sharesMinted
-    );
-    event FPMMFundingRemoved(
-        address indexed funder,
-        uint[] amountsRemoved,
-        uint collateralRemovedFromFeePool,
-        uint sharesBurnt
-    );
-    event FPMMBuy(
-        address indexed buyer,
-        uint investmentAmount,
-        uint feeAmount,
-        uint indexed outcomeIndex,
-        uint outcomeTokensBought
-    );
-    event FPMMSell(
-        address indexed seller,
-        uint returnAmount,
-        uint feeAmount,
-        uint indexed outcomeIndex,
-        uint outcomeTokensSold
-    );
-    ConditionalTokens internal conditionalTokens;
-    IERC20 internal collateralToken;
-    bytes32[] internal conditionIds;
-    uint internal fee;
-    uint internal feePoolWeight;
-
-    uint[] internal outcomeSlotCounts;
-    bytes32[][] internal collectionIds;
-    uint[] internal positionIds;
-    mapping (address => uint256) internal withdrawnFees;
-    uint internal totalWithdrawnFees;
-}
-
+//pragma solidity ^0.5.1;
+//import {FixedProductMarketMaker} from "gnosis.pm/conditional-tokens-market-makers/contracts/FixedProductMarketMaker.sol";
+//import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
+//import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+//import { ConditionalTokens } from "gnosis.pm/conditional-tokens-contracts/contracts/ConditionalTokens.sol";
+//import { CTHelpers } from "gnosis.pm/conditional-tokens-contracts/contracts/CTHelpers.sol";
+//import { ERC1155TokenReceiver } from "gnosis.pm/conditional-tokens-contracts/contracts/ERC1155/ERC1155TokenReceiver.sol";
+//import { ERC20 } from "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+//
+//contract ExtendedMarketMakert is FixedProductMarketMaker {
+//}
+//
+//// for proxying purposes
+//contract FixedProductMarketMakerData {
+//    mapping (address => uint256) internal _balances;
+//    mapping (address => mapping (address => uint256)) internal _allowances;
+//    uint256 internal _totalSupply;
+//
+//
+//    bytes4 internal constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+//    mapping(bytes4 => bool) internal _supportedInterfaces;
+//
+//
+//    event FPMMFundingAdded(
+//        address indexed funder,
+//        uint[] amountsAdded,
+//        uint sharesMinted
+//    );
+//    event FPMMFundingRemoved(
+//        address indexed funder,
+//        uint[] amountsRemoved,
+//        uint collateralRemovedFromFeePool,
+//        uint sharesBurnt
+//    );
+//    event FPMMBuy(
+//        address indexed buyer,
+//        uint investmentAmount,
+//        uint feeAmount,
+//        uint indexed outcomeIndex,
+//        uint outcomeTokensBought
+//    );
+//    event FPMMSell(
+//        address indexed seller,
+//        uint returnAmount,
+//        uint feeAmount,
+//        uint indexed outcomeIndex,
+//        uint outcomeTokensSold
+//    );
+//    ConditionalTokens internal conditionalTokens;
+//    IERC20 internal collateralToken;
+//    bytes32[] internal conditionIds;
+//    uint internal fee;
+//    uint internal feePoolWeight;
+//
+//    uint[] internal outcomeSlotCounts;
+//    bytes32[][] internal collectionIds;
+//    uint[] internal positionIds;
+//    mapping (address => uint256) internal withdrawnFees;
+//    uint internal totalWithdrawnFees;
+//}
+//
