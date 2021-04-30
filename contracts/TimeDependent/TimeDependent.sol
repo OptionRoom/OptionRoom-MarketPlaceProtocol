c8091b6f4c7f5c866f85a22b9aea3d56abc397d5
Tareq doufish <boobish@gmail.comd>
2021-05-01 02:45:53 +0300

    Change to the name of the var. Adding a new methods used in testing

--- contracts/TimeDependent/TimeDependent.sol
+++ contracts/TimeDependent/TimeDependent.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract CentralTimeForTesting{
-    
-    uint256 ct ;
+
+    uint256 crt ;
     function getCurrentTime() public view returns(uint256){
-        return ct + timeIncrease;
+        return crt + timeIncrease;
     }
-    
+
     uint256 timeIncrease;
 
     function increaseTime(uint256 t) public {
@@ -16,25 +16,32 @@ contract CentralTimeForTesting{
     function resetTimeIncrease() public {
         timeIncrease = 0;
     }
-    
+
+    function setTime(uint256 t) public {
+        crt = t;
+    }
+
+    function initializeTime() public {
+        crt = block.timestamp;
+    }
 }
 
 
 contract TimeDependent{
     CentralTimeForTesting centralTimeForTesting = CentralTimeForTesting(0x3c4Fca7B5944A750C3EBF732dBf04591aCbb821d);
-    
+
     function setCentralTimeForTesting(CentralTimeForTesting _centralTimeForTesting) public{
         centralTimeForTesting = _centralTimeForTesting;
     }
-    
+
     function getCurrentTime1() public view returns(uint256){
         require(address(centralTimeForTesting) != address(0), "central time is not set");
         return centralTimeForTesting.getCurrentTime();
         //return block.timestamp;
     }
-    
+
     function getCurrentTime() public view returns(uint256){
-       
+
         return block.timestamp;
     }
 }
