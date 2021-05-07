de3d797546d95a83c2cb60b95ee530aa728f1a9c
Tareq yousef doufish <boobish@gmail.com>
2021-05-07 13:03:43 +0300

    Option room/extending testing (#7)
    * Fixing the market orginal test.
    Adding tests to the counts of the markets.
    
    * Starting test with dispute.
    
    * Adding number.
    
    * Changes to min liquidity testings to covre edge cases.
    Chnages to dispute market.
    
    * Added common methods to facilitate the test.
    Change the markets creation to use those facilities.
    
    * Using the new way of doing some tests.
    
    * Changes to buy sell.
    
    * Using the refactored code for market creation.
    
    * Changes for the new utilities.
    
    * Chnages for new code
    
    Co-authored-by: Tareq doufish <boobish@gmail.comd>

--- test/test-market-states.js
+++ test/test-market-states.js
@@ -1,107 +1,32 @@
-var chai = require('chai');
+const ORMarketLib = artifacts.require('ORMarketLib')
 
-//use default BigNumber
-chai.use(require('chai-bignumber')());
-
-const { expectEvent } = require('openzeppelin-test-helpers')
+const {
+  prepareContracts, createNewMarket,
+  executeControllerMethod, moveToActive, conditionalApproveForAll, callControllerMethod,
+  conditionalBalanceOf, moveToResolving,resetTimeIncrease,increaseTime,moveToResolved
+} = require('./utils/market.js')
 const { toBN } = web3.utils
+var BigNumber = require('bignumber.js')
 
-let ConditionalTokensContract = artifacts.require("../../contracts/OR/ORConditionalTokens.sol");
 
-const WETH9 = artifacts.require('WETH9')
-const PredictionMarketFactoryMock = artifacts.require('PredictionMarketFactoryMock')
-const ORFPMarket = artifacts.require('ORFPMarket')
-const ORMarketController = artifacts.require('ORMarketController')
-const CentralTimeForTesting = artifacts.require('CentralTimeForTesting')
-
-var BigNumber = require('bignumber.js');
-const helper = require('ganache-time-traveler');
-
-contract('MarketMakerStates', function([, creator, oracle, investor1, trader, investor2]) {
-  let conditionalTokens
+// TODO: Tareq, please add the withdraw of the votes for the resolving, and check the power reset.
+contract('Option room: test proposal states', function([, creator, oracle, investor1, trader, investor2]) {
   let collateralToken
-  let fixedProductMarketMakerFactory
   let fixedProductMarketMaker
   let governanceMock
-  let orgTimeSnapShot
 
   let marketPendingPeriod = 1800;
-  let marketResolvingPeriod = 1800;
-  let marketDisputePeriod = 4 * 1800;
-  let marketReCastResolvingPeriod = 4 * 1800;
-
-  let disputeThreshold = toBN(100e18);
-  let minHoldingToDispute = toBN(10e18);
-
-  let centralTime;
-
-  const questionString = "Test"
-  const feeFactor = toBN(3e15) // (0.3%)
-
-  let positionIds
-  async  function createConditionalTokensContract(theDate, days) {
-    conditionalTokens = await ConditionalTokensContract.new();
-  }
-
+  
   before(async function() {
-    await createConditionalTokensContract();
-    collateralToken = await WETH9.deployed();
-    fixedProductMarketMakerFactory = await PredictionMarketFactoryMock.deployed()
-    governanceMock = await ORMarketController.deployed()
-    centralTime = await CentralTimeForTesting.deployed();
-
-    // Assign the timer to the governance.
-    await fixedProductMarketMakerFactory.setCentralTimeForTesting(centralTime.address);
-    await governanceMock.setCentralTimeForTesting(centralTime.address);
-
-    let deployedMarketMakerContract = await ORFPMarket.deployed();
-
-    await fixedProductMarketMakerFactory.setTemplateAddress(deployedMarketMakerContract.address);
-    await fixedProductMarketMakerFactory.assign(conditionalTokens.address);
-    await fixedProductMarketMakerFactory.assignCollateralTokenAddress(collateralToken.address);
-    await fixedProductMarketMakerFactory.assignGovernanceContract(governanceMock.address);
-
-    // Setting the voting power.
-    await governanceMock.setPower(investor1, 5);
-    await governanceMock.setPower(investor2, 1);
-    await governanceMock.setPower(trader, 2);
-    await governanceMock.setPower(oracle, 3);
-
-    let snapshot = await helper.takeSnapshot();
-    orgTimeSnapShot = snapshot['result'];
+    governanceMock = await prepareContracts(creator, oracle, investor1, trader, investor2)
   })
-
-  function addDays(theDate, days) {
-    return new Date(theDate.getTime() + days*24*60*60*1000);
-  }
 
   it('can be created by factory', async function() {
-    let now = new Date();
-    let resolvingEndDate = addDays(now, 5);
-    let endTime = Math.floor(addDays(now,3).getTime() / 1000);
-    let resolvingEndTime = Math.floor(resolvingEndDate.getTime() / 1000);
-
-    const createArgs = [
-      questionString,
-      endTime,
-      resolvingEndTime,
-      feeFactor,
-      { from: creator }
-    ]
-
-    await centralTime.initializeTime();
-    const fixedProductMarketMakerAddress = await fixedProductMarketMakerFactory.createMarketProposalTest.call(...createArgs)
-    const createTx = await fixedProductMarketMakerFactory.createMarketProposalTest(...createArgs);
-    expectEvent.inLogs(createTx.logs, 'FixedProductMarketMakerCreation', {
-      creator,
-      fixedProductMarketMaker: fixedProductMarketMakerAddress,
-      conditionalTokens: conditionalTokens.address,
-      collateralToken: collateralToken.address,
-    });
-
-    fixedProductMarketMaker = await ORFPMarket.at(fixedProductMarketMakerAddress)
+    let retValues = await createNewMarket(creator)
+    fixedProductMarketMaker = retValues[0]
+    collateralToken = retValues[1]
   })
-
+  
   let orgTimeStamp;
   it('Should pass because we are in the voting period', async function() {
     orgTimeStamp = governanceMock.getCurrentTime();
@@ -118,7 +43,7 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
   it('Should revert because the market is in pending state', async function() {
     const REVERT = "Market is not in validation state";
     let day = 1800 * 2;
-    await centralTime.increaseTime(day);
+    await increaseTime(day);
     try {
       await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address, true, { from: investor1 });
       throw null;
@@ -131,7 +56,7 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
 
 
   it('Should revert because address alread voted', async function() {
-    await centralTime.resetTimeIncrease();
+    await resetTimeIncrease();
     const REVERT = "user already voted";
 
     try {
@@ -145,9 +70,10 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
   });
 
   it('Should be to unvote', async function() {
-    await centralTime.resetTimeIncrease();
+    await resetTimeIncrease();
 
     await governanceMock.withdrawGovernanceValidatingVote(fixedProductMarketMaker.address, { from: investor1 });
+    // await governanceMock.withdrawGovernanceResolvingVote(fixedProductMarketMaker.address, { from: investor1 });
 
     let marketInformation = await governanceMock.getMarketInfo(fixedProductMarketMaker.address);
     let validatingVotesCount = marketInformation['validatingVotesCount'];
@@ -157,7 +83,7 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
   });
 
   it('Should roll back because address already voted', async function() {
-    await centralTime.resetTimeIncrease();
+    await resetTimeIncrease();
 
     const REVERT = "user did not vote";
 
@@ -172,10 +98,33 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
   });
 
 
-  it('Should allow multiple different governance should be able to vote', async function() {
+  it('Should allow multiple different governance to vote and check their status', async function() {
     await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address, true, { from: investor2 });
     await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address, false, { from: trader });
     await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address, false, { from: oracle });
+
+    // Checking the results of the votes.
+    let v2ValidatingVoter = await governanceMock.isValidatingVoter(fixedProductMarketMaker.address, investor2);
+    let traderValidatingVoter = await governanceMock.isValidatingVoter(fixedProductMarketMaker.address, trader);
+    let oracleValidatingVoter = await governanceMock.isValidatingVoter(fixedProductMarketMaker.address, oracle);
+    let creatorValidatingVoter = await governanceMock.isValidatingVoter(fixedProductMarketMaker.address, creator);
+
+    // We don't want to check on the third number as its insignificant to us.
+    expect(new BigNumber(v2ValidatingVoter[0]).isEqualTo(new BigNumber(1))).to.equal(true);
+    expect(v2ValidatingVoter[1]).to.equal(true);
+    expect(new BigNumber(v2ValidatingVoter[2]).isEqualTo(new BigNumber(1))).to.equal(true);
+
+    expect(new BigNumber(traderValidatingVoter[0]).isEqualTo(new BigNumber(2))).to.equal(true);
+    expect(traderValidatingVoter[1]).to.equal(true);
+    expect(new BigNumber(traderValidatingVoter[2]).isEqualTo(new BigNumber(0))).to.equal(true);
+
+    expect(new BigNumber(oracleValidatingVoter[0]).isEqualTo(new BigNumber(3))).to.equal(true);
+    expect(oracleValidatingVoter[1]).to.equal(true);
+    expect(new BigNumber(oracleValidatingVoter[2]).isEqualTo(new BigNumber(0))).to.equal(true);
+
+    expect(new BigNumber(creatorValidatingVoter[0]).isEqualTo(new BigNumber(0))).to.equal(true);
+    expect(creatorValidatingVoter[1]).to.equal(false);
+    expect(new BigNumber(creatorValidatingVoter[2]).isEqualTo(new BigNumber(0))).to.equal(true);
   });
 
 
@@ -187,14 +136,14 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
     expect(new BigNumber(validatingVotesCount[0]).isEqualTo(new BigNumber(5))).to.equal(true);
     expect(new BigNumber(validatingVotesCount[1]).isEqualTo(new BigNumber(1))).to.equal(true);
 
-    await centralTime.resetTimeIncrease();
-    await centralTime.increaseTime(marketPendingPeriod);
+    await resetTimeIncrease();
+    await increaseTime(marketPendingPeriod);
 
     // Start should be shown as rejected.
     let state = await governanceMock.getMarketState(fixedProductMarketMaker.address);
     expect(new BigNumber(state).isEqualTo(new BigNumber(2))).to.equal(true);
 
-    await centralTime.resetTimeIncrease();
+    await resetTimeIncrease();
 
     // First try to reject it
     await governanceMock.withdrawGovernanceValidatingVote(fixedProductMarketMaker.address, { from: investor2 });
@@ -206,7 +155,7 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
     await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address, true, { from: trader });
     await governanceMock.castGovernanceValidatingVote(fixedProductMarketMaker.address,true,  { from: oracle });
 
-    await centralTime.increaseTime(marketPendingPeriod);
+    await increaseTime(marketPendingPeriod);
     state = await governanceMock.getMarketState(fixedProductMarketMaker.address);
     expect(new BigNumber(state).isEqualTo(new BigNumber(3))).to.equal(true);
   });
@@ -250,12 +199,12 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
 
   let firstTimeResolve;
   it('Should be able to cast a resolving vote for gov', async function() {
-    await centralTime.resetTimeIncrease();
-    await centralTime.increaseTime(marketPendingPeriod);
+    await resetTimeIncrease();
+    await increaseTime(marketPendingPeriod);
 
     let days = ((86400 * 3) + 10);
 
-    await centralTime.increaseTime(days);
+    await increaseTime(days);
     await governanceMock.castGovernanceResolvingVote(fixedProductMarketMaker.address, 0, { from: investor1 });
   });
 
@@ -283,15 +232,30 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
     expect(new BigNumber(power).isEqualTo(new BigNumber(1))).to.equal(true);
   });
 
-  // it('Should test marketPendingVotersInfo to return the correct information', async function() {
-  //   let govPendingVotersResults = await governanceMock.marketValidatingVoters.call(investor2);
-  //   let selection = govPendingVotersResults["selection"];
-  //   let power = govPendingVotersResults["power"];
-  //
-  //   expect(new BigNumber(selection).isEqualTo(new BigNumber(1))).to.equal(true);
-  //   expect(new BigNumber(power).isEqualTo(new BigNumber(1))).to.equal(true);
-  // });
-  //
+  it('Should test marketPendingVotersInfo to return the correct information', async function() {
+    // Checking the results of the votes.
+    let v2ResolvingVoter = await governanceMock.isResolvingVoter(fixedProductMarketMaker.address, investor2);
+    let traderResolvingVoter = await governanceMock.isResolvingVoter(fixedProductMarketMaker.address, trader);
+    let oracleResolvingVoter = await governanceMock.isResolvingVoter(fixedProductMarketMaker.address, oracle);
+    let creatorResolvingVoter = await governanceMock.isResolvingVoter(fixedProductMarketMaker.address, creator);
+
+    expect(new BigNumber(v2ResolvingVoter[0]).isEqualTo(new BigNumber(1))).to.equal(true);
+    expect(v2ResolvingVoter[1]).to.equal(true);
+    expect(new BigNumber(v2ResolvingVoter[2]).isEqualTo(new BigNumber(1))).to.equal(true);
+
+    expect(new BigNumber(traderResolvingVoter[0]).isEqualTo(new BigNumber(2))).to.equal(true);
+    expect(traderResolvingVoter[1]).to.equal(true);
+    expect(new BigNumber(traderResolvingVoter[2]).isEqualTo(new BigNumber(1))).to.equal(true);
+
+    expect(new BigNumber(oracleResolvingVoter[0]).isEqualTo(new BigNumber(3))).to.equal(true);
+    expect(oracleResolvingVoter[1]).to.equal(true);
+    expect(new BigNumber(oracleResolvingVoter[2]).isEqualTo(new BigNumber(1))).to.equal(true);
+
+    expect(new BigNumber(creatorResolvingVoter[0]).isEqualTo(new BigNumber(0))).to.equal(true);
+    expect(creatorResolvingVoter[1]).to.equal(false);
+    expect(new BigNumber(creatorResolvingVoter[2]).isEqualTo(new BigNumber(0))).to.equal(true);
+  });
+
   it('Should return not resolved after the voting', async function() {
     let outcome = await governanceMock.getResolvingOutcome(fixedProductMarketMaker.address);
     // We know the market is not in resolving yet.
@@ -299,7 +263,7 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
     expect(new BigNumber(outcome[1]).isEqualTo(new BigNumber(1))).to.equal(true);
   });
 
-  it('Should check the pending voters from gov', async function() {
+  it('Should check the validating voters from gov', async function() {
     let govPendingVotersResults = await governanceMock.isResolvingVoter(fixedProductMarketMaker.address, investor2);
 
     let voteFlag = govPendingVotersResults["voteFlag"];
@@ -310,33 +274,4 @@ contract('MarketMakerStates', function([, creator, oracle, investor1, trader, in
     expect(new BigNumber(selection).isEqualTo(new BigNumber(1))).to.equal(true);
     expect(new BigNumber(power).isEqualTo(new BigNumber(1))).to.equal(true);
   });
-
-  it('Should revert because we are not in dispute period', async function() {
-    const REVERT = "Market is not in dispute state";
-
-    try {
-      await governanceMock.disputeMarket(fixedProductMarketMaker.address, "I dont want to play", { from: trader });
-      throw null;
-    }
-    catch (error) {
-      assert(error, "Expected an error but did not get one");
-      assert(error.message.includes(REVERT), "Expected '" + REVERT + "' but got '" + error.message + "' instead");
-    }
-  });
-
-  // it('Should check on the state change when voting from governance', async function() {
-  //   // Initially here we are the resolving state !
-  //   let state = await fixedProductMarketMaker.getCurrentState();
-  //   // let userBalances = await fixedProductMarketMaker.getBalances(trader);
-  //   //
-  //   // let days = ((86400 * 3) + 10);
-  //   //
-  //   // await fixedProductMarketMaker.increaseTime(days);
-  //   //
-  //   // await fixedProductMarketMaker.disputeMarket("Might just pass", { from: trader });
-  //   //
-  //   // expect(new BigNumber(state).isEqualTo(new BigNumber(5))).to.equal(true);
-  //
-  //   // Will need more on the dispute but for now I am going to leave it.
-  // });
 })
---
+++ test/utils/constants.js
@@ -0,0 +1,11 @@
+const marketValidationPeriod = 1800;
+const marketResolvingPeriod = 1800;
+const marketDisputePeriod = 4 * 1800;
+const marketReCastResolvingPeriod = 4 * 1800;
+
+module.exports = {
+  marketValidationPeriod,
+  marketResolvingPeriod,
+  marketDisputePeriod,
+  marketReCastResolvingPeriod,
+}
