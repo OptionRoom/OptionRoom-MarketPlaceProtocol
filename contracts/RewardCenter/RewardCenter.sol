9f3889f543dc1f5dbeeb25c4f33e8ae45b8ee9bd
Nasser <nassernajjar@gmail.com>
2021-05-09 14:06:30 +0300

    reward center add require msg.sender == reward program

--- contracts/RewardCenter/RewardCenter.sol
+++ contracts/RewardCenter/RewardCenter.sol
@@ -1,19 +1,34 @@
 pragma solidity ^0.5.1;
 
 import "./IRewardCenter.sol";
+import "../Guardian/GnGOwnable.sol";
 
-contract RewardCenter is IRewardCenter {
+contract RewardCenter is IRewardCenter, GnGOwnable{
+    
+    address rewardProgramAddress;
+    
+    function setRewardProgram(address programAddress) public onlyGovOrGur{
+        rewardProgramAddress = programAddress;
+    }
     
     function sendRoomReward(address beneficiary, uint256 amount, string calldata comment) external{
+        require(msg.sender == rewardProgramAddress, "only reward program allowed to send rewards");
         //todo
     }
     
     function sendRoomRewardByERC20Value(address beneficiary, uint256 amount, IERC20 erc20, string calldata comment) external{
+        require(msg.sender == rewardProgramAddress, "only reward program allowed to send rewards");
         //todo
     }
     
     // todo: gardian controll
     function sendRoomRewardByDollarAmount(address beneficiary, uint256 amount, string calldata comment) external{
-        
+        require(msg.sender == rewardProgramAddress, "only reward program allowed to send rewards");
+        //todo
+    }
+    
+    
+    function sendRoomByGovOrGur(address beneficiary, uint256 amount) public onlyGovOrGur{
+        //todo
     }
 }
